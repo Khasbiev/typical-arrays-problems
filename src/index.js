6840a98b5cafb69d8eb1a0fbac22c06280981f32
Artur Khasbiev <Khasbiich@yandex.ru>
2020-10-04 17:03:40 +0300

    init: start tasl typical arrays problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 
 exports.min = function min (array) {
-  return 0;
+  if ((typeof array === 'object')&&(array.length !== 0)){
+  return Math.min.apply(null, array)
+  }
+
+  else{ 
+  	return 0;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if ((typeof array === 'object')&&(array.length !== 0)){
+  return Math.max.apply(null, array)
+  }
+
+  else{ 
+  	return 0;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if ((typeof array === 'object')&&(array.length !== 0)){
+    let summa = 0;
+    let kolVo = 0;
+for( let i = 0; i < array.length; i++)
+{
+  summa += array[i];
+  kolVo++;
+}
+return summa/kolVo;
+  
+  }
+  else{ return 0;}
 }
